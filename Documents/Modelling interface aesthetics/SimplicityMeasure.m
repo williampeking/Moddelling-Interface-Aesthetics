7f19e79b38a04fc9e7308fa67950848a26492741
williampeking <williampeking@163.com>
2018-01-18 14:36:48 +0800

    delete old version

--- Documents/Modelling interface aesthetics/SimplicityMeasure.m
+++
@@ -1,9 +0,0 @@
-function [ SMM ] = SimplicityMeasure( n_vap, n_hap, n )
-
-% nvap and nhap are the numbers of vertical and horizontal alignment points
-% n is the number of objects on the frame.
-
-SMM = 3/(n_vap+n_hap+n);
-
-end
-
